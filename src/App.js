2cca70e9b8ea55dbc0bf58e202998503407176d8
masterdev1013 <masterdev1013@gmail.com>
2023-11-01 10:20:26 -0700

    tailwindcss

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-4">
+      <h1 className="text-3xl font-bold">Hello, Tailwind CSS!</h1>
+      <p className="mt-5">This is a sample React app using Tailwind CSS.</p>
+    </div>
+  );
+}
+
+export default App;
